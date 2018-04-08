4c59b7b3fe7db2d46c1075165fa34da6e139e259
Shqiptar <alexandre.malaj@gmail.com>
2018-04-08 17:58:15 +0200

    remove 's' for test folder for ava see:https://github.com/avajs/ava/issues/875

---
+++ test/test.js
@@ -0,0 +1,8 @@
+// Require Dependencies!
+const test = require('ava');
+const is = require('@sindresorhus/is');
+const Config = require("../src/config.class");
+
+test("first test", async(t) => {
+
+});
